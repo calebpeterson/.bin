a81ceea6fa71e883053333111338858b97254f53
Caleb Peterson <caleb.peterson@zapier.com>
2025-03-27 10:12:24 -0400

    lazily initialize the Anthropic/Braintrust client

--- lib/ai/client.ts
+++ lib/ai/client.ts
@@ -12,16 +12,19 @@ export const anthropic = new Anthropic({
   apiKey: ENV.ANTHROPIC_API_KEY,
 });
 
-const anthropicBraintrust = new OpenAI({
-  baseURL: "https://api.braintrust.dev/v1/proxy",
-  apiKey: ENV.ANTHROPIC_API_KEY,
-  defaultHeaders: {
-    "x-bt-use-cache": "never",
-  },
-});
+let anthropicBraintrust: OpenAI | undefined = undefined;
 
 export const getClient = (model: string) => {
   if (model.toLowerCase().startsWith("claude")) {
+    if (!anthropicBraintrust) {
+      anthropicBraintrust = new OpenAI({
+        baseURL: "https://api.braintrust.dev/v1/proxy",
+        apiKey: ENV.ANTHROPIC_API_KEY,
+        defaultHeaders: {
+          "x-bt-use-cache": "never",
+        },
+      });
+    }
     return anthropicBraintrust;
   }
 
